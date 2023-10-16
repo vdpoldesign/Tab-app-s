38891d761c6e9b8a9c2aed483a23c0b87b56b90d
Wouter van der Pol <woutervdpol@gmail.com>
2023-10-16 13:36:16 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+const messages = ["Leer javascript", "Leer React", "Vind een baan"];
 
 function App() {
+  const [stap, setStap] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+  function handleVorige() {
+    //if (stap > 1) setStap(stap - 1);
+    if (stap > 1) setStap((s) => s - 1);
+  }
+  function handleVolgende() {
+    if (stap > 1) setStap((s) => s + 1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button className="close" onClick={() => setIsOpen((is) => !is)}>
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={stap === 1 ? "active" : ""}>1</div>
+            <div className={stap === 2 ? "active" : ""}>2</div>
+            <div className={stap === 3 ? "active" : ""}>3</div>
+          </div>
+          <p className="message">
+            Stap {stap}: {messages[stap - 1]}
+          </p>
+          <div className="buttons">
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handleVorige}
+            >
+              Vorige
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handleVolgende}
+            >
+              Volgende
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
